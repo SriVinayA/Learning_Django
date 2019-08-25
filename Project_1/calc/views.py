42804bb3f75d252244e68d1c6159ec011c89325b
vinay <vinay@pop-os.localdomain>
2019-08-25 16:51:11 +0530

    project1

--- Project_1/calc/views.py
+++ Project_1/calc/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(requests):
-    return HttpResponse("Hello World")
+    return render(requests, 'home.html', {'name': 'Vinay'})
